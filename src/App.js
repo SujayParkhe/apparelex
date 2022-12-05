870a84fa75a13bbad9fc5e4132a3e545da4da915
sujay parkhe <sparkhe852@gmail.com>
2022-12-05 17:36:05 +0530

    Initializing apparelex project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./categories.styles.scss";
 
-function App() {
+const categories = [
+  {
+    id: 1,
+    title: "Men",
+  },
+  {
+    id: 2,
+    title: "Women",
+  },
+  {
+    id: 3,
+    title: "Kids",
+  },
+  {
+    id: 4,
+    title: "Sneakers",
+  },
+  {
+    id: 5,
+    title: "Jackets",
+  },
+  {
+    id: 6,
+    title: "Hats & Caps",
+  },
+];
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="categories-container">
+      {categories.map((category) => {
+        return (
+          <div className="category-container" key={category.id}>
+            <div className="background-image" />
+            <div className="category-body-container">
+              <h2>{category.title}</h2>
+              <p>Shop Now</p>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
-}
+};
 
 export default App;
